e94e949e5b3803d875b8d49d3ba9aae5f275bb27
Juan-Estevez <juank2001estevez@gmail.com>
2021-08-27 19:50:04 -0500

    Correcting small error 27/08/2021

--- php/view/view_vote.php
+++ php/view/view_vote.php
@@ -4,5 +4,5 @@
 <input type="radio" name="lenguaje" id="" value="java"> Java <br>
 <input type="radio" name="lenguaje" id="" value="javascript"> JavaScript <br>
 <input type="radio" name="lenguaje" id="" value="python"> Python <br>
-<input type="radio" name="lenguaje" id="" value="visual"> Visual Basic <br>
+<input type="radio" name="lenguaje" id="" value="visual_basic"> Visual Basic <br>
 <input type="submit" value="Votar!!">
